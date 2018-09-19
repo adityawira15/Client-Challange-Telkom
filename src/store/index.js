29dfe83413ef1bcaa2c0f2f598ac1d726e3afac1
ridhogusti <ridhogusti0@gmail.com>
2018-09-19 13:33:23 +0700

    delete purchase

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import Vue from 'vue'
 import Vuex from 'vuex'
-import purchase from './modules/purchase'
+// import purchase from './modules/purchase'
 import project from './modules/project'
 // component store
 Vue.use(Vuex)
 const store = new Vuex.Store({
 modules: {
-purchase,
+// purchase,
 project
 // module store
 }
